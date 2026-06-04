e3152b504c33075cacb0038cbe3f3c5845de20f6
Manikanta6878 <93917315+Manikanta6878@users.noreply.github.com>
2026-06-04 13:33:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     data = pd.read_csv("Titanic-Dataset.csv")
-  return data.head().to_string()
+    return data.head().to_string()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
