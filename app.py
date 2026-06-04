9a1edc6068c927fe70be3f77bc4e5832dd5124bb
Manikanta6878 <93917315+Manikanta6878@users.noreply.github.com>
2026-06-04 11:58:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    data = pd.read_csv("data.csv")
+    return f"Rows in dataset: {len(data)}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
